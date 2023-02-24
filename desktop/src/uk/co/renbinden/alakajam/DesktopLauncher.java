ff74d89eb3861f9115f212e985961c135a0c6ea5
Ren Binden <ren.binden@gmail.com>
2023-02-24 19:59:22 +0000

    Initial commit

---
+++ desktop/src/uk/co/renbinden/alakajam/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package uk.co.renbinden.alakajam;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import uk.co.renbinden.alakajam.Alakajam17;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Alakajam 17");
+		new Lwjgl3Application(new Alakajam17(), config);
+	}
+}
